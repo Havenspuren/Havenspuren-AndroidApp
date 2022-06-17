dd8305ce95637e6f67cb5cf6527f9f110714670d
Geist5000 <geist5000@gmail.com>
2022-06-17 16:28:30 +0200

    fixed instruction panel

--- app/src/main/java/de/jadehs/vcg/layout/fragments/information/Contributors.java
+++ app/src/main/java/de/jadehs/vcg/layout/fragments/information/Contributors.java
@@ -1,5 +1,6 @@
 package de.jadehs.vcg.layout.fragments.information;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,18 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.mukesh.MarkdownView;
-
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import de.jadehs.vcg.R;
+import de.jadehs.vcg.databinding.FragmentContributorsBinding;
+import io.noties.markwon.Markwon;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Contributors extends Fragment {
 
-    private MarkdownView markdownView;
+    private FragmentContributorsBinding binding;
 
     public Contributors() {
         //
@@ -35,15 +41,34 @@ public class Contributors extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_contributors, container, false);
-        return v;
+        binding = FragmentContributorsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final Markwon markwon = Markwon.create(requireContext());
+        String markdown;
 
-        markdownView = view.findViewById(R.id.contributor_markdown_view);
-        markdownView.loadMarkdownFromAssets("contributors.md");
+        try {
+            InputStream contributorsFile = requireContext().getAssets().open("contributors.md", AssetManager.ACCESS_BUFFER);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(contributorsFile));
+            StringBuilder markdownBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                markdownBuilder.append(line);
+                markdownBuilder.append("\n");
+            }
+            markdown = markdownBuilder.substring(0, markdownBuilder.length() - 1);
+
+        } catch (IOException e) {
+            markdown = requireContext().getString(R.string.markdown_didnt_load);
+            e.printStackTrace();
+        }
+        markwon.setMarkdown(binding.contributorMarkdownView, markdown);
+
+
     }
 }
