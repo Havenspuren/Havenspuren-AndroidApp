2017ce0f6ab788f894cdd9856997e9bdb1472c00
Geist5000 <geist5000@gmail.com>
2023-01-06 14:13:54 +0100

    added jpeg as picture type

--- app/src/main/java/de/jadehs/vcg/layout/fragments/galery/adapter/MediaPagerAdapter.kt
+++ app/src/main/java/de/jadehs/vcg/layout/fragments/galery/adapter/MediaPagerAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 
-val pictureFileEndings = listOf("png", "jpg", "bmp", "gif", "webp", "heic", "heif")
+val pictureFileEndings = listOf("png", "jpg", "jpeg", "bmp", "gif", "webp", "heic", "heif")
 val videoFileEndings = listOf("3gp", "mp4", "mkv", "mkv", "webm")
 
 class MediaPagerAdapter(uriList: List<Uri>, val fragmentManager: FragmentManager) : RecyclerView.Adapter<MediaViewHolder>() {
@@ -22,18 +22,18 @@ class MediaPagerAdapter(uriList: List<Uri>, val fragmentManager: FragmentManager
             pictureFileEndings.any {
                 path.endsWith(it, true)
             }
-        }?: false // if path not null check endsWith, false otherwise is what ?: does
+        } ?: false // if path not null check endsWith, false otherwise is what ?: does
         val isVideo = mediaUri.lastPathSegment?.let { path ->
             videoFileEndings.any {
                 path.endsWith(it, true)
             }
-        }?: false // if path not null check endsWith, false otherwise is what ?: does
+        } ?: false // if path not null check endsWith, false otherwise is what ?: does
         return if (isPicture) 0 else if (isVideo) 1 else -1
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
-        return when(viewType){
-            0 -> PictureViewHolder(parent,fragmentManager)
+        return when (viewType) {
+            0 -> PictureViewHolder(parent, fragmentManager)
             1 -> VideoViewHolder(parent)
             else -> throw IllegalStateException("View Type isn't a valid id -> media uri doesn't have a valid file type")
         }
