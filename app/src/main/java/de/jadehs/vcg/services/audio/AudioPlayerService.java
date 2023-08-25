42930c1db9e4d79a28476ad3b50a00452ea39d22
Geist5000 <geist5000@gmail.com>
2023-08-25 09:04:52 +0200

    added timeline updates

--- app/src/main/java/de/jadehs/vcg/services/audio/AudioPlayerService.java
+++ app/src/main/java/de/jadehs/vcg/services/audio/AudioPlayerService.java
@@ -1,12 +1,19 @@
 package de.jadehs.vcg.services.audio;
 
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.media3.common.Player;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.session.MediaSession;
 import androidx.media3.session.MediaSessionService;
 
-public class AudioPlayerService extends MediaSessionService implements MediaSession.Callback {
+import de.jadehs.vcg.utils.AudioUtils;
+
+public class AudioPlayerService extends MediaSessionService implements MediaSession.Callback, Player.Listener {
     /**
      * Broadcast which is send, when playback started.
      */
@@ -26,9 +33,13 @@ public class AudioPlayerService extends MediaSessionService implements MediaSess
     public static final long PLAYBACK_LOCATION_UPDATE_INTERVAL = 300;
     private static final String TAG = "AudioPlayerService";
 
-    private static int[] DISABLED_COMMANDS = {Player.COMMAND_SET_SHUFFLE_MODE, Player.COMMAND_SET_REPEAT_MODE};
-    private static int[] EXTERNAL_DISABLED_COMMANDS = {Player.COMMAND_SET_MEDIA_ITEM, Player.COMMAND_CHANGE_MEDIA_ITEMS};
+    private static final int[] DISABLED_COMMANDS = {Player.COMMAND_SET_SHUFFLE_MODE, Player.COMMAND_SET_REPEAT_MODE};
+    private static final int[] EXTERNAL_DISABLED_COMMANDS = {Player.COMMAND_SET_MEDIA_ITEM, Player.COMMAND_CHANGE_MEDIA_ITEMS};
     private MediaSession mediaSession;
+    private Handler handler;
+    private boolean isLocationUpdatesRunning;
+    private Runnable locationUpdateCallback;
+    private LocalBroadcastManager broadcastManager;
 
     @Override
     public void onCreate() {
@@ -37,16 +48,21 @@ public class AudioPlayerService extends MediaSessionService implements MediaSess
         mediaSession = new MediaSession.Builder(this, player)
                 .setCallback(this)
                 .build();
+        handler = new Handler(player.getApplicationLooper());
+        broadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
+        player.addListener(this);
     }
 
     @Nullable
     @Override
-    public MediaSession onGetSession(MediaSession.ControllerInfo controllerInfo) {
+    public MediaSession onGetSession(@NonNull final MediaSession.ControllerInfo controllerInfo) {
         return mediaSession;
     }
 
+    @NonNull
     @Override
-    public MediaSession.ConnectionResult onConnect(MediaSession session, MediaSession.ControllerInfo controller) {
+    public MediaSession.ConnectionResult onConnect(@NonNull final MediaSession session,
+                                                   @NonNull final MediaSession.ControllerInfo controller) {
 
         MediaSession.ConnectionResult result = MediaSession.Callback.super.onConnect(session, controller);
         Player.Commands.Builder commandBuilder = result.availablePlayerCommands.buildUpon()
@@ -58,12 +74,59 @@ public class AudioPlayerService extends MediaSessionService implements MediaSess
         return MediaSession.ConnectionResult.accept(result.availableSessionCommands, commandBuilder.build());
     }
 
+
+    private void startLocationUpdates() {
+        if (!isLocationUpdatesRunning) {
+            isLocationUpdatesRunning = true;
+            if (locationUpdateCallback == null) {
+                locationUpdateCallback = new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mediaSession.getPlayer().isPlaying()) {
+                            Intent intent = new Intent(AudioPlayerService.PLAYBACK_LOCATION_CHANGED_ACTION);
+                            intent.putExtra(AudioPlayerService.EXTRA_PLAYBACK_POSITION, mediaSession.getPlayer().getCurrentPosition());
+                            intent.putExtra(AudioPlayerService.EXTRA_PLAYBACK_LENGTH, mediaSession.getPlayer().getDuration());
+
+                            broadcastManager.sendBroadcast(intent);
+
+                            handler.postDelayed(this, PLAYBACK_LOCATION_UPDATE_INTERVAL);
+                        } else {
+                            isLocationUpdatesRunning = false;
+                        }
+                    }
+                };
+            }
+
+            handler.postDelayed(locationUpdateCallback, PLAYBACK_LOCATION_UPDATE_INTERVAL);
+        }
+
+    }
+
+    private void stopLocationUpdates() {
+        handler.removeCallbacks(locationUpdateCallback);
+        isLocationUpdatesRunning = false;
+    }
+
     @Override
     public void onDestroy() {
+        stopLocationUpdates();
         mediaSession.getPlayer().release();
         mediaSession.release();
         mediaSession = null;
         super.onDestroy();
 
+    }
+
+    @Override
+    public void onPlaybackStateChanged(int playbackState) {
+        if (playbackState == Player.STATE_ENDED || playbackState == Player.STATE_IDLE) {
+            if (isLocationUpdatesRunning) {
+                stopLocationUpdates();
+            }
+        } else {
+            if (!isLocationUpdatesRunning) {
+                startLocationUpdates();
+            }
+        }
     }
 }
